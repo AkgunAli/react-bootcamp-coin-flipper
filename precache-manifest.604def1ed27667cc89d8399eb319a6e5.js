0d489d543f0533dd26eba582f4a33f3e9ee70966
Ali Akgün <alii.akgunn@hotmail.com>
2020-05-25 19:44:54 +0300

    Updates

---
+++ precache-manifest.604def1ed27667cc89d8399eb319a6e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2977994b708d2c8bfa1c0f1770590df",
+    "url": "/react-bootcamp-coin-flipper/index.html"
+  },
+  {
+    "revision": "b0862fc0e61dce0ee30a",
+    "url": "/react-bootcamp-coin-flipper/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "8b60a9a2cf1eabc7d3ae",
+    "url": "/react-bootcamp-coin-flipper/static/css/main.c7df79dd.chunk.css"
+  },
+  {
+    "revision": "b0862fc0e61dce0ee30a",
+    "url": "/react-bootcamp-coin-flipper/static/js/2.b19c7fae.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-bootcamp-coin-flipper/static/js/2.b19c7fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b60a9a2cf1eabc7d3ae",
+    "url": "/react-bootcamp-coin-flipper/static/js/main.3f6a7b7c.chunk.js"
+  },
+  {
+    "revision": "6c59b876ddbabd93995f",
+    "url": "/react-bootcamp-coin-flipper/static/js/runtime-main.92333825.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/react-bootcamp-coin-flipper/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/react-bootcamp-coin-flipper/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
